a9aef2ac2263fdbb816d92dc92774c32258a700a
Ramlan Mohamed Rinos <e21336@eng.pdn.ac.lk>
2025-10-08 00:11:34 +0530

    NDK version changed

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.todo_list_app"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "29.0.14033849"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
